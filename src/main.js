c27567d7ff8e746c8c663e3df9aebab6ec2fc2a1
nemo-51 <nathan.bier51@gmail.com>
2024-11-27 00:24:45 +0100

    refactor code

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
-//import picocss from node_modules/@picocss
-import '@picocss/pico/css/pico.slate.min.css'
-import '@picocss/pico/css/pico.colors.min.css'
+// import picocss
+import "@picocss/pico/css/pico.slate.min.css"
+import "@picocss/pico/css/pico.colors.min.css"
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue"
+import App from "./App.vue"
 
-createApp(App).mount('#app')
+createApp(App).mount("#app")
