d4aa1ac43a3ba156f0f1dae12032bda0bac028cb
nemo-51 <nathan.bier51@gmail.com>
2024-11-27 12:19:45 +0100

    format with prettier

--- src/Contact.vue
+++ src/Contact.vue
@@ -1,13 +1,15 @@
 <template>
   <nav :id="props.id">
     <ul>
-    <li><h2>{{ props.data.title }}</h2></li>
-    <li v-for="link in props.data.links" :key="link">
-      <a :href="link.url">
-        <i :class="link.icon"></i>
-        {{ link.text }}
-      </a>
-    </li>
+      <li>
+        <h2>{{ props.data.title }}</h2>
+      </li>
+      <li v-for="link in props.data.links" :key="link">
+        <a :href="link.url">
+          <i :class="link.icon"></i>
+          {{ link.text }}
+        </a>
+      </li>
     </ul>
   </nav>
 </template>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import "@picocss/pico/css/pico.slate.min.css";
 import "@picocss/pico/css/pico.colors.min.css";
 
 // import RemixIcons
-import 'remixicon/fonts/remixicon.css'
+import "remixicon/fonts/remixicon.css";
 
 import { createApp } from "vue";
 import App from "./App.vue";
